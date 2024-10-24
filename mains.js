aea5a82253c66a19c0b714e2859d579f37078679
steeski <167318267+steeski@users.noreply.github.com>
2024-10-24 11:08:14 -0300

    ss

--- mains.js
+++ mains.js
@@ -1,5 +1,5 @@
 function mostraResposta (card){
-    const resposta = card.querySelector('.cartao_conteudo__resposta');
+    const resposta = card.querySelector('.cartao__conteudo__resposta');
     if (resposta.style.display === 'none'){
         resposta.style.display = 'block';
     }

a508eac1d5d936d37b6f7a36880c10af8050e211
steeski <167318267+steeski@users.noreply.github.com>
2024-10-24 11:04:10 -0300

    aaa

--- mains.js
+++ mains.js
@@ -1,9 +1,9 @@
-function mostraResposta (cartao_conteudo){
-    const resposta = cartao_conteudo.querySelector('.cartao_conteudo_respota');
-    if (resposta.style.display == 'none'){
-        resposta.style.display == 'block';
+function mostraResposta (card){
+    const resposta = card.querySelector('.cartao_conteudo_respota');
+    if (resposta.style.display === 'none'){
+        resposta.style.display = 'block';
     }
     else{
-        resposta.style.display == 'none'
+        resposta.style.display = 'none'
     }
 }
